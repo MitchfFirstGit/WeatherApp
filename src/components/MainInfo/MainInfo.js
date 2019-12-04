4e96a15abab61205611dbfbd1183113c920d8358
Artem.Chaikovskyi <Artem.Chaikovskyi@innovecs.com>
2019-12-04 12:47:59 +0200

    Add WeekDays component

--- src/components/MainInfo/MainInfo.js
+++ src/components/MainInfo/MainInfo.js
@@ -1,5 +1,7 @@
 // modules
 import React from 'react';
+import Icon from '@mdi/react';
+import { mdiWeatherWindy, mdiWaterOutline  } from '@mdi/js';
 // styles
 import styles from './styles.module.scss';
 
@@ -9,13 +11,25 @@ function MainInfo() {
             <div className={styles.city}>
                 London, UK
             </div>
-            
+
             <div className={styles.date}>
                 Mon, 10 February
             </div>
 
             <div className={styles.temperature}>
-                23 
+                23
+            </div>
+
+            <div className={styles.wrapper}>
+                <div className={styles.wind}>
+                    <Icon path={mdiWeatherWindy} size={1.2} color="white"/>
+                    8.7 m/s
+                </div>
+
+                <div className={styles.humidity}>
+                    <Icon path={mdiWaterOutline} size={1.2} color="white"/>
+                    70%
+                </div>
             </div>
         </div>
     );
--- src/components/WeekDays/WeekDays.js
+++ src/components/WeekDays/WeekDays.js
@@ -1,20 +1,24 @@
 // modules
 import React, { useState } from 'react';
+import cx from 'classnames';
 // styles
 import styles from './styles.module.scss';
 
 function WeekDays() {
     const [activeDay, setActiveDay ] = useState('Today1');
-    
+    const handleClick = ({target}) => {
+        setActiveDay(target.innerHTML)
+    };
+
     return (
             <ul className={styles.daysContainer}>
-                <li className={styles.day}>Today1</li>
-                <li className={styles.day}>Today2</li>
-                <li className={styles.day}>Today3</li>
-                <li className={styles.day}>Today4</li>
-                <li className={styles.day}>Today5</li>
-                <li className={styles.day}>Today6</li>
-                <li className={styles.day}>Today7</li>
+                <li className={cx(styles.day, { [styles.activeDay]: activeDay === 'Today1' })} onClick={handleClick} >Today1</li>
+                <li className={cx(styles.day, { [styles.activeDay]: activeDay === 'Today2' })} onClick={handleClick}>Today2</li>
+                <li className={cx(styles.day, { [styles.activeDay]: activeDay === 'Today3' })} onClick={handleClick}>Today3</li>
+                <li className={cx(styles.day, { [styles.activeDay]: activeDay === 'Today4' })} onClick={handleClick}>Today4</li>
+                <li className={cx(styles.day, { [styles.activeDay]: activeDay === 'Today5' })} onClick={handleClick}>Today5</li>
+                <li className={cx(styles.day, { [styles.activeDay]: activeDay === 'Today6' })} onClick={handleClick}>Today6</li>
+                <li className={cx(styles.day, { [styles.activeDay]: activeDay === 'Today7' })} onClick={handleClick}>Today7</li>
             </ul>
     );
 }
