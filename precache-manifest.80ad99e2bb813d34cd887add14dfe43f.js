2a57f42bf9b2faf4ebafd616f0f1576c2c0ff8e4
Artem.Chaikovskyi <Artem.Chaikovskyi@innovecs.com>
2019-12-13 14:53:33 +0200

    Updates

---
+++ precache-manifest.80ad99e2bb813d34cd887add14dfe43f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf111dc8f46fd59e7e9e258ce1199b0c",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "e0dbe276e39623dd3b4d",
+    "url": "/WeatherApp/static/css/main.d8bef0bb.chunk.css"
+  },
+  {
+    "revision": "719499e85f2ef1b3e1a4",
+    "url": "/WeatherApp/static/js/2.d48a23a1.chunk.js"
+  },
+  {
+    "revision": "e0dbe276e39623dd3b4d",
+    "url": "/WeatherApp/static/js/main.766de181.chunk.js"
+  },
+  {
+    "revision": "37c49140f7fe881522d5",
+    "url": "/WeatherApp/static/js/runtime-main.fe8e1996.js"
+  }
+]);
