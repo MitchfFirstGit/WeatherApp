3cb26565a356044fbd1fc9814caa82f8a24e6f23
Mitch <artfixmitch@gmail.com>
2019-12-15 16:47:15 +0200

    Add animation to Menu component

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import cx from 'classnames';
 import { mdiClose, mdiDelete } from '@mdi/js';
 import Icon from '@mdi/react';
+import AnimateHeight from 'react-animate-height';
 // styles
 import styles from './styles.module.scss';
 
@@ -10,59 +11,91 @@ const Menu = ({
     handleMenuClick,
     menuVisibility
 }) => {
-    const [showFavoriteCities, setShowFavoriteCities] = useState(false);
+    const [showFavoriteCities, setShowFavoriteCities] = useState(0);
+    const [showRecentlyViewedCities, setRecentlyViewedCities] = useState(0);
+
+    const handleFavoriteCitiesClick = () => {
+        setShowFavoriteCities(showFavoriteCities === 0 ? 'auto' : 0);
+    }
+
+    const handleRecentlyViewedCitiesClick = () => {
+        setRecentlyViewedCities(showRecentlyViewedCities === 0 ? 'auto' : 0);
+    }
 
     return (
         <>
-            < div className={cx(styles.menuContainer, { [styles.menuContainerOpen]: true })}>
-                <div className={styles.closeBtn}>
-                    <Icon path={mdiClose} size={1} />
+            <div className={cx(styles.menuContainer, { [styles.menuContainerOpen]: menuVisibility })}>
+                <Icon path={mdiClose} size={1} className={styles.closeIcon} onClick={handleMenuClick} />
 
-                    <ul className={styles.menuList}>
-                        <li className={styles.item}>
-                            Favorite cities list
+                <ul className={styles.menuList}>
+                    <li className={styles.item} onClick={handleFavoriteCitiesClick}>
+                        Favorite cities list
+                    </li>
+
+                    <AnimateHeight
+                        duration={300}
+                        height={showFavoriteCities}
+
+                    >
+                        <div className={styles.dropDown}>
+                            <div className={styles.city}>
+                                City1
+
+                                <Icon path={mdiDelete} />
+                            </div>
+
+                            <div className={styles.city}>
+                                City2
+
+                                <Icon path={mdiDelete} />
+                            </div>
+
+                            <div className={styles.city}>
+                                City3
+
+                                <Icon path={mdiDelete} />
+                            </div>
+                        </div>
+                    </AnimateHeight>
+
+                    <li className={styles.item} onClick={handleRecentlyViewedCitiesClick}>
+                        Recently viewed cities
                         </li>
 
-                        <li>
-                            <ul className={styles.favoriteCitiesDropDown}>
-                                <li className={styles.favoriteCity}>
-                                    City1
+                    <AnimateHeight
+                        duration={300}
+                        height={showRecentlyViewedCities}
+                    >
+                        <div className={styles.dropDown}>
+                            <div className={styles.city}>
+                                City1
 
-                                    <Icon path={mdiDelete} />
-                                </li>
+                                <Icon path={mdiDelete} />
+                            </div>
 
-                                <li className={styles.favoriteCity}>
-                                    City2
+                            <div className={styles.city}>
+                                City2
 
-                                    <Icon path={mdiDelete} />
-                                </li>
+                                <Icon path={mdiDelete} />
+                            </div>
 
-                                <li className={styles.favoriteCity}>
-                                    City3
+                            <div className={styles.city}>
+                                City3
 
-                                    <Icon path={mdiDelete} />
-                                </li>
-                            </ul>
-                        </li>
+                                <Icon path={mdiDelete} />
+                            </div>
+                        </div>
+                    </AnimateHeight>
 
+                    <li className={styles.item}>
+                        Enable dark mode
+                    </li>
 
-                        <li className={styles.item}>
-                            Recently viewed cities
-                        </li>
-
-                        <li className={styles.item}>
-                            Enable dark mode
-                        </li>
-
-                        <li className={styles.item}>
-                            Install app
-                        </li>
-                    </ul>
-                </div>
-
-                hello
-            </div >
-
+                    <li className={styles.item}>
+                        Install app
+                    </li>
+                </ul>
+            </div>
         </>
     );
 }
