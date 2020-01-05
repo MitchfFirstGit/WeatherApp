9ecbe452ca1eb2c606f59bb26a6b7f43194ba5a0
Mitch <artfixmitch@gmail.com>
2020-01-05 23:40:59 +0200

    Change content in FirstInteraction component

--- src/components/FirstInteraction/FirstInteraction.js
+++ src/components/FirstInteraction/FirstInteraction.js
@@ -6,7 +6,7 @@ import styles from './styles.module.scss';
 const FirstInteraction = () => {
     return (
         <div className={styles.container}>
-            <h1 className={styles.header}>Welcome to PWA Weather App</h1>
+            <h1 className={styles.header}>Welcome to {/*PWA*/}  Weather App</h1>
 
             <p className={styles.text}>
                 Just type your sity in the search bar and find out what's the weather today.
@@ -19,11 +19,11 @@ const FirstInteraction = () => {
                 The list of favorite cities is in the menu. The menu also has a list of recently viewed cities
             </p>
 
-            <p className={styles.text}>
+            {/* <p className={styles.text}>
                 You can use this application without Internet connection.
                 This application will take up a small amount of memory on your device,
                 so just install it by clicking the <span className={styles.install}>Install button</span> in the menu.
-            </p>
+            </p> */}
         </div>
     );
 }
